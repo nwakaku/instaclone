d348f8001341c0697eae9ab1d1a81b8f5ecdf797
Wisdom Nwakaku <nwakakucc@gmail.com>
2022-10-02 16:52:23 +0100

    all

--- components/Navbar.js
+++ components/Navbar.js
@@ -19,15 +19,15 @@ function Navbar() {
               InstaClone
             </h1>
           </div>
-          <form class="flex items-center text-gray-700 ">
-            <label for="simple-search" class="sr-only">
+          <form className="flex items-center text-gray-700 ">
+            <label for="simple-search" className="sr-only">
               Search
             </label>
-            <div class="relative w-full">
-              <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+            <div className="relative w-full">
+              <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                 <svg
                   aria-hidden="true"
-                  class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
@@ -42,7 +42,7 @@ function Navbar() {
               <input
                 type="text"
                 id="simple-search"
-                class="bg-gray-10 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-200 dark:border-none dark:placeholder-gray-400 dark:text-gray dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-10 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-200 dark:border-none dark:placeholder-gray-400 dark:text-gray dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Search"
                 required
               />
